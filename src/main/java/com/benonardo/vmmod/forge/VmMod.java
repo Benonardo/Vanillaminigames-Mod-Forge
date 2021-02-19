26d12aee992b153cec1736d6136928375c11f47f
booky10 <github@booky.tk>
2021-02-19 17:34:42 +0100

    more readability improvements!

--- src/main/java/com/benonardo/vmmod/forge/VmMod.java
+++ src/main/java/com/benonardo/vmmod/forge/VmMod.java
@@ -1,12 +1,10 @@
 package com.benonardo.vmmod.forge;
 
-import com.benonardo.vmmod.forge.init.VmItems;
+import com.benonardo.vmmod.forge.init.Items;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("vmmod")
@@ -16,14 +14,14 @@ public class VmMod {
 
     public VmMod() {
         MinecraftForge.EVENT_BUS.register(this);
-        VmItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        Items.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     public static final ItemGroup VM_GROUP = new ItemGroup("vanillaminigames") {
 
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(VmItems.TOMATO.get());
+            return new ItemStack(Items.TOMATO.get());
         }
     };
 }
