62419220352bd037d23b9a0dd22355f7970b534d
booky10 <github@booky.tk>
2021-02-23 13:41:11 +0100

    made the tomato shit always edible

--- src/main/java/com/benonardo/vmmod/forge/items/TomatoShit.java
+++ src/main/java/com/benonardo/vmmod/forge/items/TomatoShit.java
@@ -12,7 +12,7 @@ public class TomatoShit extends Item {
     private static final EffectInstance POISON_EFFECT = new EffectInstance(Effects.POISON, 30 * 20, 2);
     private static final EffectInstance HUNGER_EFFECT = new EffectInstance(Effects.HUNGER, 30 * 20, 2);
     private static final EffectInstance DAMAGE_EFFECT = new EffectInstance(Effects.INSTANT_DAMAGE, 1, 1);
-    private static final Food food = new Food.Builder().hunger(1).saturation(0.0f).effect(() -> NAUSEA_EFFECT, 1.0f).effect(() -> HUNGER_EFFECT, 1.0f).effect(() -> POISON_EFFECT, 1.0f).effect(() -> DAMAGE_EFFECT, 1.0f).build();
+    private static final Food food = new Food.Builder().hunger(1).saturation(0.0f).effect(() -> NAUSEA_EFFECT, 1.0f).effect(() -> HUNGER_EFFECT, 1.0f).effect(() -> POISON_EFFECT, 1.0f).effect(() -> DAMAGE_EFFECT, 1.0f).setAlwaysEdible().build();
 
     public TomatoShit() {
         super(new Item.Properties().group(VmMod.VM_GROUP).food(food));
